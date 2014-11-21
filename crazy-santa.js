4144e5609e208d37a074fed67ce372c9967de675
Thomas Leplus <thomasleplus@users.noreply.github.com>
2014-11-21 21:41:59 +0100

    Update crazy-santa.js

--- crazy-santa.js
+++ crazy-santa.js
@@ -27,8 +27,29 @@
  *
  */
 
+function getParam(name) {
+    if (window.location.search == null || window.location.search.length == 0) {
+      return null;
+    }
+    var params = window.location.search.substring(1).split('&');
+    for (var i = 0; i < params.length; i++) {
+        var param = params[i].split('=');
+        if (param[0] == name) {
+            return param[1];
+        }
+    }
+    return null;
+}
+
 function start() {
-  $( "#main_content" ).html("Hello world!");
+  var output;
+  var step = getParam("step");
+  if (step == null || step == 0) {
+    output = "Hello world!";
+  } else {
+    output = "Sorry the script encountered an unexpected error!";
+  }
+  $( "#main_content" ).html(output);
 }
 
 $( document ).ready(function() {
